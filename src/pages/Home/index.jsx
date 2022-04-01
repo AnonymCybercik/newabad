96b3477649ad23299b8872c7afbaa17d76dd9026
BaxtiyorErkinov <baxa10th@gmail.com>
2022-04-01 20:46:30 +0500

    footer

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -4,22 +4,22 @@ import Banner from "../../components/Banner"
 import Products from "../../components/Products"
 import "./index.scss"
 import Categories from "../../components/Categories"
+import Footer from '../../components/Footer'
 
 function index() {
   return (
     <div>
-        <Header></Header>
-        <Banner></Banner>
-
-        <section className="sections">
-            <Categories></Categories>
-            <Products></Products>
-        </section>
-
-        <br />
-        <br />
-        <br />
-        <br />
+      <Header></Header>
+      <Banner></Banner>
+      <section className="sections">
+        <Categories></Categories>
+        <Products></Products>
+      </section>
+      <br />
+      <br />
+      <br />
+      <br />
+      <Footer />
     </div>
   )
 }
